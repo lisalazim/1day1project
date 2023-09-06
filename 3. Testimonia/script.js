acb5a748dfd9c1df2f20b42c5d8bd504a40dcb66
lisalazim <lisalazi21@gmail.com>
2023-09-06 16:58:42 +0700

    day 1 revision

--- 3. Testimonia/script.js
+++ 3. Testimonia/script.js
@@ -3,7 +3,7 @@ let box2 = document.getElementById("box2");
 let box3 = document.getElementById("box3");
 let box4 = document.getElementById("box4");
 
-function clickRight() {
+function clickRight1() {
     if (box2.style.display === "none") {
         box2.style.display = "block";
         box1.style.display = "none";
@@ -18,7 +18,7 @@ function clickRight() {
     }
 }
 
-function clickRight1() {
+function clickRight2() {
     if (box3.style.display === "none") {
         box3.style.display = "block";
         box2.style.display = "none";
@@ -30,7 +30,7 @@ function clickRight1() {
     }
 }
 
-function clickRight2() {
+function clickRight3() {
     if (box4.style.display === "none") {
         box4.style.display = "block"
         box3.style.display = "none";
@@ -44,7 +44,7 @@ function clickRight2() {
     }
 }
 
-function clickRight3() {
+function clickRight4() {
     if (box1.style.display === "none") {
         box4.style.display = "none"
         box3.style.display = "none";
@@ -58,3 +58,58 @@ function clickRight3() {
     }
 }
 
+function clickLeft1() {
+    if (box4.style.display === "none") {
+        box2.style.display = "none";
+        box1.style.display = "none";
+        box3.style.display = "none";
+        box4.style.display = "block";
+
+    } else {
+        box2.style.display = "none";
+        box1.style.display = "block";
+        box3.style.display = "none";
+        box4.style.display = "none";
+    }
+}
+
+function clickLeft2() {
+    if (box1.style.display === "none") {
+        box3.style.display = "none";
+        box2.style.display = "none";
+        box1.style.display = "block";
+    } else {
+        box3.style.display = "none";
+        box2.style.display = "block";
+        box1.style.display= "none";
+        box4.style.display = "none";
+    }
+}
+
+function clickLeft3() {
+    if (box2.style.display === "none") {
+        box4.style.display = "none"
+        box3.style.display = "none";
+        box2.style.display = "block";
+        box1.style.display = "none";
+    } else {
+        box3.style.display = "block";
+        box2.style.display = "none";
+        box1.style.display = "none";
+        box4.style.display = "none"
+    }
+}
+
+function clickLeft4() {
+    if (box3.style.display === "none") {
+        box4.style.display = "none"
+        box3.style.display = "block";
+        box2.style.display = "none";
+        box1.style.display = "none";
+    } else {
+        box3.style.display = "none";
+        box2.style.display = "none";
+        box1.style.display = "none";
+        box4.style.display = "block";
+    }
+}
